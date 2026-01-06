33c63d1094afc62379b886bdfd4305e7efcd9742
Yaakov Abramov <Yaakov.Abramov1@gmail.com>
2026-01-06 13:14:45 +0200

    now project runs and output a black image with the width and height specified in program arguments in the path given in program args

--- ray_tracer.py
+++ ray_tracer.py
@@ -10,6 +10,7 @@ from surfaces.cube import Cube
 from surfaces.infinite_plane import InfinitePlane
 from surfaces.sphere import Sphere
 
+
 def parse_camera_and_one_sphere(scene_path: str):
     camera = None
     sphere = None
@@ -54,66 +55,82 @@ def parse_camera_and_one_sphere(scene_path: str):
 
     return camera, sphere
 
-# def parse_scene_file(file_path):
-#     objects = []
-#     camera = None
-#     scene_settings = None
-#     with open(file_path, 'r') as f:
-#         for line in f:
-#             line = line.strip()
-#             if not line or line.startswith("#"):
-#                 continue
-#             parts = line.split()
-#             obj_type = parts[0]
-#             params = [float(p) for p in parts[1:]]
-#             if obj_type == "cam":
-#                 camera = Camera(params[:3], params[3:6], params[6:9], params[9], params[10])
-#             elif obj_type == "set":
-#                 scene_settings = SceneSettings(params[:3], params[3], params[4])
-#             elif obj_type == "mtl":
-#                 material = Material(params[:3], params[3:6], params[6:9], params[9], params[10])
-#                 objects.append(material)
-#             elif obj_type == "sph":
-#                 sphere = Sphere(params[:3], params[3], int(params[4]))
-#                 objects.append(sphere)
-#             elif obj_type == "pln":
-#                 plane = InfinitePlane(params[:3], params[3], int(params[4]))
-#                 objects.append(plane)
-#             elif obj_type == "box":
-#                 cube = Cube(params[:3], params[3], int(params[4]))
-#                 objects.append(cube)
-#             elif obj_type == "lgt":
-#                 light = Light(params[:3], params[3:6], params[6], params[7], params[8])
-#                 objects.append(light)
-#             else:
-#                 raise ValueError("Unknown object type: {}".format(obj_type))
-#     return camera, scene_settings, objects
+
+def parse_scene_file(file_path):
+    objects = []
+    camera = None
+    scene_settings = None
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            parts = line.split()
+            obj_type = parts[0]
+            params = [float(p) for p in parts[1:]]
+            if obj_type == "cam":
+                camera = Camera(params[:3], params[3:6], params[6:9], params[9], params[10])
+            elif obj_type == "set":
+                scene_settings = SceneSettings(params[:3], params[3], params[4])
+            elif obj_type == "mtl":
+                material = Material(params[:3], params[3:6], params[6:9], params[9], params[10])
+                objects.append(material)
+            elif obj_type == "sph":
+                sphere = Sphere(params[:3], params[3], int(params[4]))
+                objects.append(sphere)
+            elif obj_type == "pln":
+                plane = InfinitePlane(params[:3], params[3], int(params[4]))
+                objects.append(plane)
+            elif obj_type == "box":
+                cube = Cube(params[:3], params[3], int(params[4]))
+                objects.append(cube)
+            elif obj_type == "lgt":
+                light = Light(params[:3], params[3:6], params[6], params[7], params[8])
+                objects.append(light)
+            else:
+                raise ValueError("Unknown object type: {}".format(obj_type))
+    return camera, scene_settings, objects
 
 
-def save_image(image_array):
+def save_image(image_array, output_path_and_file_name):
     image = Image.fromarray(np.uint8(image_array))
 
     # Save the image to a file
-    image.save("scenes/Spheres.png")
+    image.save(output_path_and_file_name)
 
 def main():
-    camera, sphere = parse_camera_and_one_sphere("scenes/one_sphere.txt")
-    print("Camera:", camera.position)
-    print("Sphere:", sphere.position)
-#     parser.add_argument('--height', type=int, default=500, help='Image height')
-#     args = parser.parse_args()
+    # handling program arguments:
+    parser = argparse.ArgumentParser(description='Python Ray Tracer')
+    parser.add_argument('scene_file', type=str, help='Path to the scene file')
+    parser.add_argument('output_image', type=str, help='Name of the output image file')
+    parser.add_argument('--width', type=int, default=500, help='Image width')
+    parser.add_argument('--height', type=int, default=500, help='Image height')
+    args = parser.parse_args()
+    
+    # testing args parsing values:
+    print("\nArgsuments:", args, "\n\n")
 
-#     # Parse the scene file
-#     camera, scene_settings, objects = parse_scene_file(args.scene_file)
 
-#     # TODO: Implement the ray tracer
+    # Parse the scene file:
+    # FIX THIS ---> camera, scene_settings, objects = parse_scene_file(args.scene_file)
+    
 
-#     # Dummy result
-#     image_array = np.zeros((500, 500, 3))
+    # TODO: Implement the ray tracer
+    
 
-#     # Save the output image
-#     save_image(image_array)
+    # creating 2d image array of the resulting image:
+    image_array = np.zeros((args.height, args.width, 3))
+
+    # Save the output into an image:
+    save_image(image_array, args.output_image)
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
